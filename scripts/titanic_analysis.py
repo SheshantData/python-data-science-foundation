0d3f1f0eea5c215a5ad7b3d99998e3ae1f29d8db
Sheshant Mane <166479242+SheshantData@users.noreply.github.com>
2026-02-07 11:05:45 +0530

    Update titanic_analysis.py

--- scripts/titanic_analysis.py
+++ scripts/titanic_analysis.py
@@ -6,3 +6,6 @@ df = pd.read_csv('data/titanic.csv')
 # 2. Look at the data
 print("--- First 5 Passengers ---")
 print(df.head())
+
+# 3. Checking Missing Values
+print("Missing values per column:\n", df.isnull().sum())
