19c7bded74a185cfa99101a5d7042fec601edf09
Sheshant Mane <166479242+SheshantData@users.noreply.github.com>
2026-02-19 21:59:28 +0530

    Create ml_model.py

---
+++ scripts/ml_model.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+print("Machine Learning environment ready!")
+# Next step: Split data and train the model
