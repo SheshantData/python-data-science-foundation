63a5e9e061ed3537011a5be3aacb1abcf9cc874a
Sheshant Mane <166479242+SheshantData@users.noreply.github.com>
2026-02-22 11:02:00 +0530

    Update house_prediction.py

--- scripts/house_prediction.py
+++ scripts/house_prediction.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
+
 # Load the data
 df = pd.read_csv('data/Housing.csv')
 
