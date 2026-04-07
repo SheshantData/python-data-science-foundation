0c86e79ec4a774987bea17553a7d591303711657
Sheshant Mane <166479242+SheshantData@users.noreply.github.com>
2026-04-07 20:34:23 +0530

    Clean up imports in iris_classification.py
    Remove unused imports from iris_classification.py

--- scripts/iris_classification.py
+++ scripts/iris_classification.py
@@ -1,11 +1,1 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import os
+
