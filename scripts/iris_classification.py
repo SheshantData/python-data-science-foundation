db7545acb46436b9a4623205282de49e4797f520
Sheshant Mane <166479242+SheshantData@users.noreply.github.com>
2026-04-07 20:34:44 +0530

    Add necessary imports for iris classification

--- scripts/iris_classification.py
+++ scripts/iris_classification.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
